c92a0eb4a44287f2cd8046f7e3ccb60891d5993d
chocopc123 <0215ryuuku@gmail.com>
2024-04-28 18:55:15 +0900

    サーバー順にソートを行う処理を修正

--- src/sortHelpers.js
+++ src/sortHelpers.js
@@ -1,49 +1,16 @@
-function parkingNumberSort(parkingInfo) {
-  const serverList = [];
-  parkingInfo.forEach((rowData) => {
-    serverList.push(rowData[1]);
-  });
-  const uniqueServerList = [...new Set(serverList)];
-  const length = uniqueServerList.length;
-  // サーバーリスト内で自鯖を先頭にする
-  uniqueServerList.forEach((rowData, index) => {
-    if (rowData === "自鯖") {
-      const temp = uniqueServerList[0];
-      uniqueServerList[0] = uniqueServerList[index];
-      uniqueServerList[index] = temp;
-    }
-  });
+// 自鯖を先頭に移動して、他鯖との間に改行用データを仕込む処理
+function moveTopOwnServer(parkingInfos) {
+  const ownedParking = parkingInfos.filter(
+    (parkingInfo) => parkingInfo.serverName === "自鯖"
+  );
+  const anotherParking = parkingInfos.filter(
+    (parkingInfo) => parkingInfo.serverName !== "自鯖"
+  );
 
-  // サーバーリストの並び替え
-  let swapped;
-  do {
-    swapped = false;
-    for (i = 1; i < length - 1; i++) {
-      if (uniqueServerList[i] > uniqueServerList[i + 1]) {
-        const temp = uniqueServerList[i];
-        uniqueServerList[i] = uniqueServerList[i + 1];
-        uniqueServerList[i + 1] = temp;
-        swapped = true;
-      }
-    }
-  } while (swapped);
-
-  // サーバーリスト順にpush
-  const sortedParkingInfo = [];
-  uniqueServerList.forEach((serverName) => {
-    const array = [];
-    parkingInfo.forEach((rowData) => {
-      if (rowData[1] === serverName) {
-        array.push(rowData);
-      }
-    });
-    sortedParkingInfo.push(array);
-  });
-
-  return sortedParkingInfo;
+  return [...ownedParking, { isIndent: true }, ...anotherParking];
 }
 
-// 停戦時間順にソートを行う
+// 停戦時間順にソートを行う処理
 function sortByOpenTime(parkingInfos) {
   // parkingInfosを停戦時間順に並び替える
   const length = parkingInfos.length;
@@ -67,13 +34,31 @@ function sortByOpenTime(parkingInfos) {
     }
   } while (swapped);
 
-  // 自鯖とそれ以外を分離する
-  const ownedParking = parkingInfos.filter(
-    (parkingInfo) => parkingInfo.serverName === "自鯖"
-  );
-  const anotherParking = parkingInfos.filter(
-    (parkingInfo) => parkingInfo.serverName !== "自鯖"
-  );
+  // 自鯖情報を上部に移動して返却
+  return moveTopOwnServer(parkingInfos);
+}
 
-  return [...ownedParking, { isIndent: true }, ...anotherParking];
+// サーバー順にソートを行う処理
+function sortByServer(parkingInfos) {
+  // parkingInfosをサーバー順に並び替える
+  const length = parkingInfos.length;
+  let swapped;
+  do {
+    swapped = false;
+    for (i = 0; i < length - 1; i++) {
+      if (
+        (parkingInfos[i].serverName === "自鯖" &&
+          parkingInfos[i + 1] !== "自鯖") ||
+        parkingInfos[i].serverName > parkingInfos[i + 1].serverName
+      ) {
+        const temp = parkingInfos[i];
+        parkingInfos[i] = parkingInfos[i + 1];
+        parkingInfos[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+
+  // 自鯖情報を上部に移動して返却
+  return moveTopOwnServer(parkingInfos);
 }
